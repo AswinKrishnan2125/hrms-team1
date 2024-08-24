565bf412c4a4bfd708922fa79c1d07d9bcaf6b74
mayurimahadik24 <mayurishamal24@gmail.com>
2024-08-24 18:42:43 +0530

    p2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import EmployeeDetails from './components/Profile.jsx';
 
 // import { Directions } from '@mui/icons-material';
 
-import Department from './components/Department.jsx'
+
 import QuickLinks from './components/QuickLinks.jsx';
 import EmployeeTable from './components/Directory.jsx';
 import DepartmentTable from './components/Department.jsx';
